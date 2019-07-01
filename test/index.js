2c2025d6800c4dc010f84326e890e248f349cae1
John Kreisher <jkreish@gmail.com>
2019-06-30 22:42:08 -0400

    Got tests running.

--- test/index.js
+++ test/index.js
@@ -8,8 +8,8 @@ process.on('unhandledRejection', error => {
   console.error('got unhandledRejection:', error);
 });
 
-const dnaPath = path.join(__dirname, "../dist/generic-game.dna.json")
-const dna = Diorama.dna(dnaPath, 'generic-game')
+const dnaPath = path.join(__dirname, "../dist/connect-4.dna.json")
+const dna = Diorama.dna(dnaPath, 'connect-4')
 
 const diorama = new Diorama({
   instances: {
@@ -22,8 +22,7 @@ const diorama = new Diorama({
   middleware: backwardCompatibilityMiddleware,
 })
 
-
-diorama.registerScenario("Can create a new game", async (s, t, {alice, bob}) => {
+diorama.registerScenario("Can create a new game", async (s, t, { alice, bob }) => {
   const create_game_result = await alice.callSync('main', 'create_game', {
     opponent: bob.agentId,
     timestamp: 0,
@@ -36,22 +35,22 @@ diorama.registerScenario("Can create a new game", async (s, t, {alice, bob}) => 
   const move_1_result = await bob.callSync('main', 'make_move', {
     new_move: {
       game: create_game_result.Ok,
-      move_type: {DropPiece { column: 0 }},
+      move_type: {DropPiece: { column: 0 }},
       timestampe: 1,
     }
-  })
+  });
 
   t.equal(move_1_result.Err, undefined);
 
   const move_2_result = await alice.callSync('main', 'make_move', {
     new_move: {
       game: create_game_result.Ok,
-      move_type: {DropPiece { column: 8 }},
+      move_type: {DropPiece: { column: 8 }},
       timestampe: 2,
     }
-  })
-
+  });
+  
   t.equal(move_2_result.Ok, undefined);
-}
+});
 
 diorama.run()

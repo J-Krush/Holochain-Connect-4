bae9ada2de8b6a633e49aaf10cf37e7d693dc6c0
John Kreisher <jkreish@gmail.com>
2019-07-02 12:10:23 -0400

    Serving react UI's

--- ui-src/src/index.js
+++ ui-src/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { connect } from '@holochain/hc-web-client'
-function _extends() {_extends = Object.assign || function (target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i];for (var key in source) {if (Object.prototype.hasOwnProperty.call(source, key)) {target[key] = source[key];}}}return target;};return _extends.apply(this, arguments);}const { Component } = React;
+function _extends() {_extends = Object.assign || function (target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i];for (var key in source) {if (Object.prototype.hasOwnProperty.call(source, key)) {target[key] = source[key];}}}return target;};return _extends.apply(this, arguments);}
+const { Component } = React;
 
 class Container extends Component {
   constructor(props) {
     super(props);
     this.state = {
       board: this.createBoard(),
+      // holochainConnection: connect('ws://localhost:3001'),
       holochainConnection: connect(), // use when letting the conductor auto-select. Allows for multiple agents
       connected: false,
       user: {},
@@ -23,6 +25,24 @@ class Container extends Component {
     this.arrows = this.arrows.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.resetState = this.resetState.bind(this);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+
+    this.state.holochainConnection.then(({ callZome, call }) => {
+      this.setState({ connected: true })
+      callZome('connect-4', 'main', 'whoami')({}).then((result) => {
+        console.log('result: ', result);
+      })
+      .catch ((e) => {
+        console.log('error: ', e);
+      });
+    });
+    // this.makeHolochainCall('connect-4/main/whoami', (result) => {
+    //   console.log('who am i? ', result)
+    // })
   }
 
   createBoard() {
@@ -77,17 +97,20 @@ class Container extends Component {
 
   handleClick(colIndex) {
     this.setState(prevState => {
-      const board = this.updateBoard(prevState, colIndex);
 
-      const gameOver = this.checkScore(board, prevState.playerOneTurn);
-
-      return {
-        board,
-        playerOneTurn: !this.state.playerOneTurn,
-        lastColClicked: colIndex,
-        gameOver };
-
-    });
+      this.makeMove(colIndex);
+    //   const board = this.updateBoard(prevState, colIndex);
+    //
+    //   const gameOver = this.checkScore(board, prevState.playerOneTurn);
+    //
+    //   return {
+    //     board,
+    //     playerOneTurn: !this.state.playerOneTurn,
+    //     lastColClicked: colIndex,
+    //     gameOver };
+    //
+    // });
+    })
   }
 
   arrows() {
@@ -120,31 +143,44 @@ class Container extends Component {
     setTimeout(this.resetState, 1400);
   }
 
+  onClick() {
+    console.log('clicked start game button');
+  };
+
   // ******************** HOLOCHAIN STUFF ******************** //
 
-  // makeMove: ({ text, column }) => {
-  //       const message = {
-  //         // game: Address,
-  //       	// move_type: MoveType,
-  //       	// timestamp: u32,
-  //         game: Address,
-  //       	move_type: MoveType,
-  //       	timestamp: Math.floor(Date.now() / 1000),
-  //       }
+  // startGame(opponentAddress) {
+  //   this.makeHolochainCall('connect-4/main/new_game', {
+  //     address: opponentAddress,
+  //   }, (result) => {
+  //     console.log('created game', result)
+  //   })
+  // }
+  //
+  // makeMove(column) {
+  //       // const message = {
+  //       //   // game: Address,
+  //       // 	// move_type: MoveType,
+  //       // 	// timestamp: u32,
+  //       //   game: Address,
+  //       // 	move_type: MoveType,
+  //       // 	timestamp: Math.floor(Date.now() / 1000),
+  //       // }
   //       this.makeHolochainCall('connect-4/main/make_move', {
-  //         stream_address: roomId,
-  //         message
+  //         DropPiece: {
+  //           column: column,
+  //         }
   //       }, (result) => {
   //         console.log('move made', result)
   //       })
-  //     },
-
-  makeHolochainCall (callString, params, callback) {
-    const [instanceId, zome, func] = callString.split('/')
-    this.state.holochainConnection.then(({ callZome }) => {
-      callZome(instanceId, zome, func)(params).then((result) => callback(JSON.parse(result)))
-    })
-  }
+  //     }
+  //
+  // makeHolochainCall (callString, params, callback) {
+  //   const [instanceId, zome, func] = callString.split('/')
+  //   this.state.holochainConnection.then(({ callZome }) => {
+  //     callZome(instanceId, zome, func)(params).then((result) => callback(JSON.parse(result)))
+  //   })
+  // }
 
 
   // ****************** END OF HOLOCHAIN STUFF ****************** //
@@ -152,7 +188,7 @@ class Container extends Component {
   render() {
     return (
       React.createElement("div", null,
-      React.createElement(Header, { playerOneTurn: this.state.playerOneTurn, gameOver: this.state.gameOver, handleReset: this.handleReset }),
+      React.createElement(Header, { playerOneTurn: this.state.playerOneTurn, gameOver: this.state.gameOver, onClick: this.startGame }),
       React.createElement("div", { className: "container" },
       React.createElement(Row, { style: { height: '20px', marginBottom: '40px', marginLeft: '20px' } },
       this.arrows()),
@@ -161,10 +197,12 @@ class Container extends Component {
 
 
 
-  }}
+  }
+
+}
 
 
-const Header = ({ playerOneTurn, gameOver, handleReset }) => {
+const Header = ({ playerOneTurn, gameOver, onClick, agentAddress = '', gameAddress = '' }) => {
   return (
     React.createElement("div", null,
     !gameOver ?
@@ -172,14 +210,22 @@ const Header = ({ playerOneTurn, gameOver, handleReset }) => {
     React.createElement("h1", { className: "title" }, "HC Connect 4"),
     React.createElement("h1", { className: playerOneTurn ? 'player-change' : 'hide-player' }, "Player ", React.createElement("span", { className: "red" }, "One"), " Go!"),
     React.createElement("h1", { className: !playerOneTurn ? 'player-change' : 'hide-player' }, "Player ", React.createElement("span", { className: "black" }, "Two"), " Go!")) :
-
-
     React.createElement("h1", { className: "game-over" }, gameOver.split('player').join('player ')),
 
-    React.createElement(Reset, { handleReset: handleReset, lever: false })));
+    React.createElement("p1", { className: "subtitle" }, "Start game with opponent address: ", agentAddress),
+
+    React.createElement('input', { placeholder: 'Opponent Address', type: 'string', autoFocus: true }),
+
+    React.createElement( "button", { onClick: onClick, disabled: false }, "Start Game"),
+
+    React.createElement("h1", { className: "subtitle" }, "Agent Address: ", agentAddress),
+    React.createElement("h1", { className: "subtitle" }, "Game Address: ", gameAddress)));
+    // React.createElement(Reset, { handleReset: handleReset, lever: false })));
 
 
 };
+
+
 
 const Board = ({ board, reset, handleReset }) => {
   const rows = (_, i) => {
@@ -253,7 +299,7 @@ const Reset = ({ handleReset, lever }) => {
     lever ?
     React.createElement("div", { className: "lever-container" },
     React.createElement("div", { className: "lever-text" }, "Reset ",
-    React.createElement("i", { class: "fa fa-arrow-right", "aria-hidden": "true" })),
+    React.createElement("i", { className: "fa fa-arrow-right", "aria-hidden": "true" })),
 
     React.createElement("div", { className: "lever", onClick: handleReset })) :
 
---
+++ ui/precache-manifest.222ceb4d1e1328bf54688592d1d8dab7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d064878785af34ea39b1721a7aa032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b90df0172ac49a2fbbe",
+    "url": "/static/js/2.7424d0e6.chunk.js"
+  },
+  {
+    "revision": "d459a060154be21c38ec",
+    "url": "/static/js/main.d2a4ed58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
